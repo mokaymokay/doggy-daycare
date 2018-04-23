6b143e4beb7530af857f20e89e8df26431c18163
Kay Mok <kaymok3@gmail.com>
2018-04-23 17:30:09 -0400

    Add database

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Owner.create(name: "Barack Obama", email: "barack@obama.com", gender: "male")
+Owner.create(name: "Ellen Degeneres", email: "ellen@degeneres.com", gender: "female")
+Dog.create(name: "Bo", breed: "Portuguese Water Dog", age: 9, owner_id: 1)
+Dog.create(name: "Sunny", breed: "Portuguese Water Dog", age: 5, owner_id: 1)
+Dog.create(name: "Woolf", breed: "Maltese-Poodle mix", age: 6, owner_id: 2)
+Dog.create(name: "Mabel", breed: "Poodle", age: 4, owner_id: 2)
+Dog.create(name: "Augie", breed: "Jack Russell mix", age: 3, owner_id: 2)
+Dog.create(name: "Kid", breed: "Terrier mix", age: 1, owner_id: 2)

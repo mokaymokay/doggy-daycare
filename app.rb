0d5bb2095b0dc4d4f2cd3de5cfe1d748eac84539
Kay Mok <kaymok3@gmail.com>
2018-04-23 23:55:39 -0400

    Add routes

--- app.rb
+++ app.rb
@@ -4,3 +4,23 @@ require_relative './models/Dog'
 require_relative './models/Owner'
 
 set :database, {adapter: 'postgresql', database: 'doggydaycare'}
+
+get '/' do
+  erb :index
+end
+
+get '/dogs' do
+  @dogs = Dog.all
+  erb :dogs
+end
+
+get '/owners' do
+  @owners = Owner.all
+  erb :owners
+end
+
+get '/owners/:id' do
+  @owner = Owner.find(params[:id])
+  @dogs = Dog.where(owner_id: params[:id])
+  erb :owner
+end
